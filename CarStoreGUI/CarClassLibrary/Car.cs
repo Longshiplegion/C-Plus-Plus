592a0f09e19aa2819729769325cd6a21765fec06
Andrew Myers <103467846+Longshiplegion@users.noreply.github.com>
2023-06-09 09:45:31 -0500

    Add files via upload

---
+++ CarStoreGUI/CarClassLibrary/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStoreGUI
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public String Model { get; set; }
+        public decimal Price { get; set; }
+        public int Miles { get; set; }
+        public int Year { get; set; }
+
+
+
+        public Car(string make,string model,decimal price,int miles,int year)
+        {
+            Make = make;
+            Model = model;
+            Price = price;
+            Miles = miles;
+            Year = year;
+        }
+
+        public Car()
+        {
+            Make = "Nothing Yet";
+            Model = "Nothing Yet";
+            Price = 0;
+            Year = 0;
+            Miles = 0;
+        }
+        public string Display
+        {
+            get
+            {
+                return string.Format("{0} {1} ${2} {3} {4}", Make, Model, Price,Miles,Year);
+
+            }
+        }
+
+    }
+}
---
+++ CarStoreGUI/CarStoreGUI/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarStoreGUI
+{
+    public partial class Form1 : Form
+    {
+        Store store = new Store();
+        BindingSource carListBinding = new BindingSource();
+        BindingSource shoppingListBinding = new BindingSource();
+        public Form1()
+        {
+            InitializeComponent();
+            setBindings();
+        }
+
+        public void setBindings()
+        {
+            carListBinding.DataSource = store.CarList;
+            listBox1.DataSource = carListBinding;
+            listBox1.DisplayMember = "Display";
+            listBox1.ValueMember = "Display";
+
+            shoppingListBinding.DataSource = store.ShoppingList;
+            listBox2.DataSource = shoppingListBinding;
+            listBox2.DisplayMember = "Display";
+            listBox2.ValueMember = "Display";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Car newCar = new Car();
+                newCar.Make = textBox1.Text;
+                newCar.Model = textBox2.Text;
+                newCar.Price = Decimal.Parse(textBox3.Text);
+                newCar.Miles = int.Parse(textBox5.Text);
+                newCar.Year = int.Parse(textBox4.Text);
+                store.CarList.Add(newCar);
+            }
+            catch (Exception ex)
+            {
+               
+            }
+            carListBinding.ResetBindings(false);
+            }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            store.ShoppingList.Add((Car)listBox1.SelectedItem);
+            shoppingListBinding.ResetBindings(false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            decimal total = store.checkout();
+            label5.Text = total.ToString();
+        }
+    }
+}
