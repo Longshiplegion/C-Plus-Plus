4a5e067aa036d3a8bbece9e742dc394efe01ab42
Andrew Myers <103467846+Longshiplegion@users.noreply.github.com>
2023-06-09 09:48:54 -0500

    Add files via upload

---
+++ Milestone 1/Milestone 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Milestone_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            double difficulty = 0.1;
+
+            Board board = new Board(size);
+            board.SetupLiveNeighbors(difficulty);
+            board.CalculateLiveNeighbors();
+
+            PrintBoard(board);
+        }
+
+        static void PrintBoard(Board board)
+        {
+            for (int row = 0; row < board.Size; row++)
+            {
+                for (int col = 0; col < board.Size; col++)
+                {
+                    Cell cell = board.Grid[row, col];
+                    if (cell.Live)
+                    {
+                        Console.Write("[*]");
+                    }
+                    else if (cell.Visited)
+                    {
+                        Console.Write("[X]");
+                    }
+                    else
+                    {
+                        Console.Write("[ ]");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Milestone2/Milestone2/Cell.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Milestone2
+{
+    public class Cell
+    {
+        private int row = -1;
+        private int col = -1;
+        private bool visited = false;
+        private bool live = false;
+        private int liveNeighbors = 0;
+
+        public Cell(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+        public int Row
+        {
+            get { return row; }
+            set { row = value; }
+        }
+
+        public int Col
+        {
+            get { return col; }
+            set { col = value; }
+        }
+
+        public bool Visited
+        {
+            get { return visited; }
+            set { visited = value; }
+        }
+
+        public bool Live
+        {
+            get { return live; }
+            set { live = value; }
+        }
+
+        public int LiveNeighbors
+        {
+            get { return liveNeighbors; }
+            set { liveNeighbors = value; }
+        }
+    }
+
+}
