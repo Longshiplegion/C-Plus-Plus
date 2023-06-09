32c89720a59771e6e68122282d235f70368c51dc
Andrew Myers <103467846+Longshiplegion@users.noreply.github.com>
2023-06-09 09:47:15 -0500

    Add files via upload

---
+++ ChessBoardGuiApp/ChessBoardGuiApp/Form1.cs
@@ -0,0 +1,92 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoardGuiApp
+{
+    public partial class Form1 : Form
+    {
+        static public Board myBoard = new Board(8);
+        public Button[,] bttnGrid = new Button[myBoard.Size, myBoard.Size];
+        public Form1()
+        {
+            InitializeComponent();
+            populateGrid();
+        }
+        public void populateGrid()
+        {
+            int buttonSize = panel1.Width / myBoard.Size;
+            panel1.Height = panel1.Width;
+
+            for(int r = 0; r < myBoard.Size; r++)
+            {
+                for (int c = 0; c < myBoard.Size; c++)
+                {
+                    bttnGrid[r, c] = new Button();
+                    bttnGrid[r, c].Width = buttonSize;
+                    bttnGrid[r, c].Height = buttonSize;
+                    bttnGrid[r, c].Click += Grid_Button_Click;
+                    panel1.Controls.Add(bttnGrid[r, c]);
+                    bttnGrid[r, c].Location = new Point(buttonSize * r, buttonSize * c);
+                    bttnGrid[r, c].Tag = r.ToString() + "|" + c.ToString();
+                }
+            }
+        }
+
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a chess piece.");
+                return;
+            }
+            String[] strArr = (sender as Button).Tag.ToString().Split("|");
+            int r, c;
+            try
+            {
+                r = int.Parse(strArr[0]);
+                c = int.Parse(strArr[1]);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Invalid button tag format: " + ex.Message);
+                return;
+            }
+            if (r >= 0 && r < myBoard.Size && c >= 0 && c < myBoard.Size)
+            {
+                Cell currentCell = myBoard.theGrid[r, c];
+                myBoard.MarkNextLegalMove(currentCell, comboBox1.Text);
+                updateButtonLabels();
+
+                for (int i = 0; i < myBoard.Size; i++)
+                {
+                    for (int j = 0; j < myBoard.Size; j++)
+                    {
+                        bttnGrid[i, j].BackColor = default(Color);
+                    }
+                }
+                    (sender as Button).BackColor = Color.Cornsilk;
+            }
+            
+        }
+        public void updateButtonLabels()
+        {
+            for (int r = 0; r < myBoard.Size; r++)
+            {
+                for (int c = 0; c < myBoard.Size; c++)
+                {
+                    bttnGrid[r, c].Text = "";
+                    if (myBoard.theGrid[r, c].CurrentlyOccupied) bttnGrid[r, c].Text = comboBox1.Text;
+                    if (myBoard.theGrid[r, c].LegalNextMove) bttnGrid[r, c].Text = "Legal";
+                }
+            }
+        }
+    }
+}
---
+++ ChessBoardGuiApp/ChessBoardModel/Board.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel
+{
+    public class Board
+    {
+        public int Size { get; set; }
+        public Cell[,] theGrid;
+        public Board(int s)
+        {
+            Size = s;
+            theGrid = new Cell[Size, Size];
+            for(int i = 0; i < Size; i++)
+            {
+                for(int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public void MarkNextLegalMove(Cell currentCell,string chessPiece)
+        {
+            for(int r = 0; r < Size; r++)
+            {
+                for(int c = 0; c < Size; c++)
+                {
+                    theGrid[r, c].LegalNextMove = false;
+                }
+            }
+
+            try
+            {
+                switch (chessPiece)
+                {
+                    case "Knight":
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                        break;
+                    case "King":
+                        // code for legal moves of King
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                        break;
+                    case "Rook":
+                        // code for legal moves of Rook
+                        for (int i = currentCell.RowNumber + 1; i < 8; i++)
+                        {
+                            if (theGrid[i, currentCell.ColumnNumber].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, currentCell.ColumnNumber].LegalNextMove = true;
+                        }
+                        for (int i = currentCell.RowNumber - 1; i >= 0; i--)
+                        {
+                            if (theGrid[i, currentCell.ColumnNumber].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, currentCell.ColumnNumber].LegalNextMove = true;
+                        }
+                        for (int j = currentCell.ColumnNumber + 1; j < 8; j++)
+                        {
+                            if (theGrid[currentCell.RowNumber, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[currentCell.RowNumber, j].LegalNextMove = true;
+                        }
+                        for (int j = currentCell.ColumnNumber - 1; j >= 0; j--)
+                        {
+                            if (theGrid[currentCell.RowNumber, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[currentCell.RowNumber, j].LegalNextMove = true;
+                        }
+                        break;
+                    case "Bishop":
+                        // code for legal moves of Bishop
+                        for (int i = currentCell.RowNumber + 1, j = currentCell.ColumnNumber + 1; i < 8 && j < 8; i++, j++)
+                        {
+                            if (theGrid[i, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, j].LegalNextMove = true;
+                        }
+                        for (int i = currentCell.RowNumber + 1, j = currentCell.ColumnNumber - 1; i < 8 && j >= 0; i++, j--)
+                        {
+                            if (theGrid[i, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, j].LegalNextMove = true;
+                        }
+                        for (int i = currentCell.RowNumber - 1, j = currentCell.ColumnNumber + 1; i >= 0 && j < 8; i--, j++)
+                        {
+                            if (theGrid[i, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, j].LegalNextMove = true;
+                        }
+                        for (int i = currentCell.RowNumber - 1, j = currentCell.ColumnNumber - 1; i >= 0 && j >= 0; i--, j--)
+                        {
+                            if (theGrid[i, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, j].LegalNextMove = true;
+                        }
+                        break;
+                    case "Queen":
+                        // code for legal moves of Queen
+                        // code for legal moves of Bishop
+                        for (int i = currentCell.RowNumber + 1, j = currentCell.ColumnNumber + 1; i < 8 && j < 8; i++, j++)
+                        {
+                            if (theGrid[i, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, j].LegalNextMove = true;
+                        }
+                        for (int i = currentCell.RowNumber + 1, j = currentCell.ColumnNumber - 1; i < 8 && j >= 0; i++, j--)
+                        {
+                            if (theGrid[i, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, j].LegalNextMove = true;
+                        }
+                        for (int i = currentCell.RowNumber - 1, j = currentCell.ColumnNumber + 1; i >= 0 && j < 8; i--, j++)
+                        {
+                            if (theGrid[i, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, j].LegalNextMove = true;
+                        }
+                        for (int i = currentCell.RowNumber - 1, j = currentCell.ColumnNumber - 1; i >= 0 && j >= 0; i--, j--)
+                        {
+                            if (theGrid[i, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, j].LegalNextMove = true;
+                        }
+                        // code for legal moves of Rook
+                        for (int i = currentCell.RowNumber + 1; i < 8; i++)
+                        {
+                            if (theGrid[i, currentCell.ColumnNumber].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, currentCell.ColumnNumber].LegalNextMove = true;
+                        }
+                        for (int i = currentCell.RowNumber - 1; i >= 0; i--)
+                        {
+                            if (theGrid[i, currentCell.ColumnNumber].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[i, currentCell.ColumnNumber].LegalNextMove = true;
+                        }
+                        for (int j = currentCell.ColumnNumber + 1; j < 8; j++)
+                        {
+                            if (theGrid[currentCell.RowNumber, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[currentCell.RowNumber, j].LegalNextMove = true;
+                        }
+                        for (int j = currentCell.ColumnNumber - 1; j >= 0; j--)
+                        {
+                            if (theGrid[currentCell.RowNumber, j].CurrentlyOccupied)
+                            {
+                                break;
+                            }
+                            theGrid[currentCell.RowNumber, j].LegalNextMove = true;
+                        }
+                        break;
+                }
+            }catch(Exception e)
+            { }
+        }
+
+
+
+    }
+        }
+    
